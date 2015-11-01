7221147c9678d4d337de110760982291e98409e7
Tore Banta <bantat@carleton.edu>
2015-11-01 16:18:21 -0600

    It works!

--- interpreter.c
+++ interpreter.c
@@ -11,6 +11,44 @@
 #include "value.h"
 #include "parser.h"
 
+void printVal(Value *input) {
+    switch (input->type) {
+        case CONS_TYPE:
+            printf("(");
+            printVal(car(input));
+            printf(", ");
+            printVal(cdr(input));
+            printf(")");
+            break;
+        case BOOL_TYPE:
+            if (input->i == 0) {
+                printf("#f:boolean");
+            }
+            else {
+                printf("#t:boolean");
+            }
+            break;
+        case INT_TYPE:
+            printf("%i:integer", input->i);
+            break;
+        case DOUBLE_TYPE:
+            printf("%f:float", input->d);
+            break;
+        case STR_TYPE:
+            printf("\"%s\":string", input->s);
+            break;
+        case SYMBOL_TYPE:
+            printf("%s:symbol", input->s);
+            break;
+        case PTR_TYPE:
+            printf("%i:pointer", input->p);
+            break;
+        case NULL_TYPE:
+            printf("null");
+            break;
+    }
+}
+
 void interpret(Value *tree) {
     Frame *global = talloc(sizeof(Frame));
     while ((*tree).type != NULL_TYPE) {
@@ -40,28 +78,50 @@ void interpret(Value *tree) {
                 printf("%s\n", (*result).s);
                 break;
         }
-        
         tree = cdr(tree);
     }
 }
 
-void evaluationError() {
-    printf("Evaluation error\n");
+void evaluationError(int error) {
+    printf("Evaluation error: ");
+    if (error == 0) {
+        printf("Test condition for if statement doesn't resolve solve to a boolean\n");
+    }
+    else if (error == 1) {
+        printf("If statement doesn't contain enough arguments\n");
+    }
+    else if (error == 2) {
+        printf("Parameter for Let must be nested list\n");
+    }
+    else if (error == 3) {
+        printf("Symbol being evaluated doesn't have a bound value\n");
+    }
+    else if (error == 4) {
+        printf("Not a recognized special form\n");
+    }
+    else if (error == 5) {
+        printf("Not a symbol\n");
+    }
     texit(1);
 }
 
 Value *lookUpSymbol(Value *symbol, Frame *frame) {
     while (frame != NULL) {
         Value *bindings = frame->bindings;
-        /*printf("\n");
-        printTree(bindings);*/
+        while (bindings == NULL) {
+            if (frame->parent == NULL) {
+                evaluationError(3);
+            }
+            frame = frame->parent;
+            bindings = frame->bindings;
+        }
         while ((*bindings).type != NULL_TYPE) {
             Value *symbol1_cons = car(bindings);
             Value *symbol1 = car(symbol1_cons);
             assert(symbol1->type == SYMBOL_TYPE);
-            if ((*symbol1).s == (*symbol).s) {
-                assert (car(cdr(symbol1_cons))->type == PTR_TYPE);
-                return (*car(cdr(symbol1_cons))).p;
+            
+            if (strcmp((*symbol1).s, (*symbol).s) == 0) {
+                return car(cdr(symbol1_cons));
             }
             else {
                 bindings = cdr(bindings);
@@ -69,16 +129,24 @@ Value *lookUpSymbol(Value *symbol, Frame *frame) {
         }
         frame = frame->parent;
     }
-    evaluationError();
+    evaluationError(3);
     return symbol;
 }
 
 Value *evalIf(Value *args, Frame *frame) {
-    assert(args->type == CONS_TYPE);
-    assert(cdr(args)->type == CONS_TYPE);
-    assert(cdr(cdr(args))->type == CONS_TYPE);
+    if (args->type != CONS_TYPE){
+        evaluationError(1);
+    }
+    else {
+        if (cdr(args)->type != CONS_TYPE) {
+            evaluationError(1);
+        }
+        else if (cdr(cdr(args))->type != CONS_TYPE) {
+            evaluationError(1);
+        }
+    }
     
-    Value *bool_exp = eval(car(args),frame);
+    Value *bool_exp = eval(car(args), frame);
     Value *true_result = car(cdr(args));
     Value *false_result = car(cdr(cdr(args)));
     if (bool_exp->type == BOOL_TYPE) {
@@ -90,29 +158,48 @@ Value *evalIf(Value *args, Frame *frame) {
         }
     }
     else {
-        evaluationError();
+        evaluationError(0);
     }
     return args;
 }
 
 Value *evalLet(Value *args, Frame *frame) {
-    /*printf("\n");
-    printTree(args);
-    printf("\n");*/
     Frame *new_frame = talloc(sizeof(Frame));
     new_frame->parent = frame;
-    Value *new_bindings = car(args);
+    Value *new_bindings = makeNull();
+    
+    Value *cur_node = car(args);
+    
+    if (car(cur_node)->type != CONS_TYPE) {
+        evaluationError(2);
+    }
+    
+    while (cur_node->type != NULL_TYPE) {
+        Value *node_to_eval = car(cdr(car(cur_node)));
+        Value *pointer = eval(node_to_eval, frame);
+ 
+        Value *val = cons(pointer,makeNull());
+        Value *symbol_val = car(car(cur_node));
+        if (symbol_val->type == SYMBOL_TYPE) {
+            val = cons(symbol_val, val);
+        }
+        else {
+            texit(1);
+        }
+        new_bindings = cons(val, new_bindings);
+        
+        cur_node = cdr(cur_node);
+    }
+    
     
     new_frame->bindings = new_bindings;
-    printTree(new_bindings);
-    printf("\nWeird error above\n\n\n");
-    //assert(new_bindings->type != NULL_TYPE);
-    return eval(car(cdr(args)),new_frame);
+
+    return eval(car(cdr(args)), new_frame);
 }
 
 Value *eval(Value *tree, Frame *frame) {
     Value *result;
-    switch (tree->type)  {
+    switch (tree->type) {
         case INT_TYPE:
             result = tree;
             break;
@@ -126,7 +213,7 @@ Value *eval(Value *tree, Frame *frame) {
             result = tree;
             break;
         case SYMBOL_TYPE:
-            result =  lookUpSymbol(tree, frame);
+            result = lookUpSymbol(tree, frame);
             break;
         case CONS_TYPE:
             {
@@ -136,25 +223,24 @@ Value *eval(Value *tree, Frame *frame) {
             // Sanity and error checking on first...
             
             if ((*first_arg).type != SYMBOL_TYPE) {
-                evaluationError();
+                evaluationError(5);
             }
             
-            if (strcmp(first_arg->s,"if")==0) {
+            if (strcmp(first_arg->s,"if") == 0) {
                 result = evalIf(args, frame);
             }
             
-            else if (strcmp(first_arg->s, "let")==0) {
+            else if (strcmp(first_arg->s, "let") == 0) {
                 result = evalLet(args, frame);
             }
             
             else {
                 // not a recognized special form
-                evaluationError();
+                evaluationError(4);
             }
             break;
             }
         // more case statements?
     }
-    // do something here
     return result;
 }
